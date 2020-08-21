4cb5b82b79db918bd90be100c74f90aaba9f0e32
RathodVipul234 <58567554+RathodVipul234@users.noreply.github.com>
2020-08-21 15:43:25 +0530

    Delete PythonCodeForWhatsappMsg.py

--- PythonCodeForWhatsappMsg.py
+++
@@ -1,6 +0,0 @@
-from twilio.rest import Client
-client =Client()
-FromWhatsappNumber = 'whatsapp:+917283876336'
-ToWhatsappNumber = 'whatsapp:+919664876062'
-
-client.messages.create(body = "Hey Gandu",from_ = FromWhatsappNumber,to = ToWhatsappNumber)
